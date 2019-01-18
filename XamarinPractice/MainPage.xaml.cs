8ddf54b06af2edfa7cc7391b5f3a9797411255f0
chooyan-eng <chooyan.engineer@gmail.com>
2019-01-18 12:28:54 +0900

    [modify] add tricks to update ListView's UI automatically

--- XamarinPractice/MainPage.xaml.cs
+++ XamarinPractice/MainPage.xaml.cs
@@ -1,5 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.ComponentModel;
 using Xamarin.Forms;
 
@@ -7,7 +7,7 @@ namespace XamarinPractice
 {
     public partial class MainPage : ContentPage
     {
-        private List<Schedule> ScheduleListData = new List<Schedule>();
+        private ObservableCollection<Schedule> ScheduleListData = new ObservableCollection<Schedule>();
         public MainPage()
         {
             InitializeComponent();
@@ -21,14 +21,54 @@ namespace XamarinPractice
             // ListViewにデータソースをセット
             scheduleList.ItemsSource = ScheduleListData;
         }
+
+        public void ChangeFirstTitle(object sender, EventArgs e)
+        {
+            ScheduleListData[0].Title = "The Concert is Canceled!!!";
+        }
+
+        public void AppendNewData(object sender, EventArgs e)
+        {
+            ScheduleListData.Add(new Schedule("2019/01/29", "Enjoy Onsen Ryokan in Chichibu", "Chichibu, Saitama"));
+        }
     }
 
     // リスト1件のデータを表すクラス
-    public class Schedule
+    public class Schedule : INotifyPropertyChanged
     {
-        public string Datestr { get; set; }
-        public string Title { get; set; }
-        public string Place { get; set; }
+        private string datestr;
+        public string Datestr
+        {
+            get => this.datestr;
+            set
+            {
+                this.datestr = value;
+                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(Datestr)));
+            }
+        }
+
+        private string title;
+        public string Title
+        {
+            get => this.title;
+            set
+            {
+                this.title = value;
+                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(Title)));
+            }
+        }
+
+        private string place;
+        public string Place
+        {
+            get => this.place;
+            set
+            {
+                this.place = value;
+                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(Place)));
+            }
+        }
+
 
         public Schedule(string Datestr, string Title, string Place)
         {
@@ -36,5 +76,7 @@ namespace XamarinPractice
             this.Title = Title;
             this.Place = Place;
         }
+
+        public event PropertyChangedEventHandler PropertyChanged;
     }
 }
